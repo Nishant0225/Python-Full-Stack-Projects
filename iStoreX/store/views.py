56e16e9a196f73c39e7c163fcce7a4c311f9967e
Nishant0225 <nishantkadu7020@gmail.com>
2026-01-21 02:48:31 +0530

    add views for signup and forgot password templates

--- iStoreX/store/views.py
+++ iStoreX/store/views.py
@@ -32,5 +32,9 @@ def login_view(request):
             })
 
     return render(request, "login.html")
+
 def signup_view(request):
     return render(request, "signup.html")
+
+def forgot_password_view(request):
+    return render(request, "forgot_password.html")
