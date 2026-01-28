c3600c0f238740d80895bb00de5ef81f352e4178
Nishant0225 <nishantkadu7020@gmail.com>
2026-01-28 05:32:06 +0530

    Update apps.py

--- iStoreX/store/apps.py
+++ iStoreX/store/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
-
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+    def ready(self):
+        from .models import Category
+        if not Category.objects.exists():
+            Category.objects.create(name="iPhone")
+            Category.objects.create(name="Mac")
+            Category.objects.create(name="iPad")
+            Category.objects.create(name="AirPods")
