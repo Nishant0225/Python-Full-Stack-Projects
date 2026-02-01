de6701a86f4d21a99a8cb652e9a9b19d5882c9ef
Nishant0225 <nishantkadu7020@gmail.com>
2026-02-02 02:01:13 +0530

    made certifications view and url

--- Personal_portfolio/Portfolio/urls.py
+++ Personal_portfolio/Portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('certifications/', views.certifications, name='certifications'),
 ]
