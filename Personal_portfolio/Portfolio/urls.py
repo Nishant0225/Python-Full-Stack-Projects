d47883909d841cc62b8cb73e8abd663ce84b59ce
Nishant0225 <nishantkadu7020@gmail.com>
2026-02-04 01:25:48 +0530

    education url added

--- Personal_portfolio/Portfolio/urls.py
+++ Personal_portfolio/Portfolio/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('certifications/', views.certifications, name='certifications'),
+    path('education/', views.education, name='education'),
 ]
