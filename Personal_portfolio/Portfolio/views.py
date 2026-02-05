af7befb4490a28fe73a425eb1004af9583a93cc3
Nishant0225 <nishantkadu7020@gmail.com>
2026-02-06 02:17:22 +0530

    added import statements

--- Personal_portfolio/Portfolio/views.py
+++ Personal_portfolio/Portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.conf import settings
 # Create your views here.
 def index(request):
     return render(request,'index.html')
