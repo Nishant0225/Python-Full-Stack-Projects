593676844bd5789cbd173921bbac02c75746dd32
Nishant0225 <nishantkadu7020@gmail.com>
2026-02-06 02:37:57 +0530

    updated views

--- Personal_portfolio/Portfolio/views.py
+++ Personal_portfolio/Portfolio/views.py
@@ -1,4 +1,4 @@
-
+import requests
 from django.shortcuts import render, redirect
 from django.conf import settings
 # Create your views here.
@@ -13,3 +13,34 @@ def certifications(request):
 
 def education(request):
     return render(request, 'education.html')
+
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+CHAT_ID = "YOUR_CHAT_ID_HERE"
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        text = f"""
+📩 New Contact Message
+
+👤 Name: {name}
+📧 Email: {email}
+
+💬 Message:
+{message}
+        """
+
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+
+        requests.post(url, data=payload)
+
+        return redirect("index")
+
+    return render(request, "contact.html")
