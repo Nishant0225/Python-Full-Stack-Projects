241d6b984ff49a1daa0a93323bd92f4dd377699d
Nishant0225 <nishantkadu7020@gmail.com>
2026-02-06 02:28:28 +0530

    DELETED UNUSED STUFF

--- Personal_portfolio/Personal_portfolio/settings.py
+++ Personal_portfolio/Personal_portfolio/settings.py
@@ -124,10 +124,3 @@ STATICFILES_DIRS = [BASE_DIR/"static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = "nishantkadu7020@gmail.com"
-EMAIL_HOST_PASSWORD = "YOUR_GMAIL_APP_PASSWORD"
